3c7c1f8d78a90d2c888b6b6f1e37bf3fd4a73b1e
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2023-05-08 21:19:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const finalDate = new Date("July 09, 2023 12:00:00").getTime();
+
+const countdownInterval = setInterval(() => {
+  const currentDate = new Date().getTime();
+
+  const remainingTime = finalDate - currentDate;
+
+  const totalSeconds = Math.floor(remainingTime / 1000);
+  const daysValue = Math.floor(totalSeconds / 86400);
+  const hoursValue = Math.floor((totalSeconds % 86400) / 3600);
+  const minutesValue = Math.floor((totalSeconds % 3600) / 60);
+  const secondsValue = totalSeconds % 60;
+
+  days.innerHTML = daysValue;
+  hours.innerHTML = hoursValue;
+  minutes.innerHTML = minutesValue;
+  seconds.innerHTML = secondsValue;
+
+  if (remainingTime < 0) {
+    days.innerHTML = 0;
+    hours.innerHTML = 0;
+    minutes.innerHTML = 0;
+    seconds.innerHTML = 0;
+    clearInterval(countdownInterval);
+    const defaults = {
+      spread: 300,
+      ticks: 200,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+      shapes: ["heart"],
+      colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+    };
+
+    confetti({
+      ...defaults,
+      particleCount: 50,
+      scalar: 2,
+    });
+
+    confetti({
+      ...defaults,
+      particleCount: 25,
+      scalar: 3,
+    });
+
+    confetti({
+      ...defaults,
+      particleCount: 10,
+      scalar: 4,
+    });
+
+    function shoot() {
+      confetti({
+        ...defaults,
+        particleCount: 40,
+        scalar: 1.2,
+        shapes: ["star"],
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 0.5,
+        shapes: ["circle"],
+      });
+    }
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+  }
+});
